e4ccff4dcc0cce10495ddad28f1e9bd44152d734
Sean P. Cornelius <spcornelius@gmail.com>
2020-11-06 16:59:16 -0500

    Fix conflicting settings causing non-deterministic outcomes despite manual seed

--- scripts/util.py
+++ scripts/util.py
@@ -11,5 +11,5 @@ def set_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     np.random.seed(seed)
-    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
