6c7a85f6d12f0fb013e22faa375cfa2608eb87c0
micha <mmdanziger@gmail.com>
2021-01-22 00:09:12 +0200

    Added mode 4 (noodle mode) UNTESTED
    Mode 4 is the noodle mode. It has a parameter K
    which describes the ratio of contact sites in a cluster
    to its mass. Any cluster with a ratio below K is blue.
    This mode requires a convolution to solve quickly
    so I added dependency on scipy. Will test soon.

---
+++ blue_zero/env/mode4.py
@@ -0,0 +1,49 @@
+import numpy as np
+from scipy.signal import convolve2d
+
+from blue_zero.clusters import find_clusters
+from blue_zero.config import Status
+from blue_zero.env.base import BlueBase
+
+__all__ = []
+__all__.extend([
+    'BlueMode4'
+])
+
+
+class BlueMode4(BlueBase):
+    '''
+    This is the noodle mode. 
+    A component turns blue if the ratio of black contacts to size
+    drops below K (the noodle dimension)
+    '''
+
+
+    def __init__(self, *args, K=2.0,**kwargs):
+        '''
+        K is the noodle dimension, the ratio of boundary contacts
+        to cluster size below which the cluster becomes blue
+        '''
+        self.K = K
+        super().__init__(*args, **kwargs)
+        
+
+    def update(self):
+        s = self.state
+        not_blocked = np.logical_or(s == Status.alive, s == Status.dead)
+        clusters, sizes = find_clusters(not_blocked)    
+        touching_filter = np.array([[0,1,0],[1,0,1],[0,1,0]])
+        #all sites are boundary sites or `not_blocked so we negate`
+        boundary_contacts = convolve2d(~not_blocked, touching_filter, 
+                        mode="same",boundary="fill")
+        boundary_contacts *= not_blocked
+        #count total contacts per cluster (can this be vectorized?)
+        cluster_contacts = np.zeros_like(sizes)
+        for i in range(self.state.shape[0]):
+            for j in range(self.state.shape[1]):
+                cluster_contacts[clusters[i,j]]+=boundary_contacts[i,j]
+
+        idx = (cluster_contacts[clusters] / sizes[clusters] <= self.K)
+        self.state[idx] = Status.dead
+        self._game_over = np.all(self.state != Status.alive)
+        super().update()
--- blue_zero/env/util.py
+++ blue_zero/env/util.py
@@ -1,16 +1,18 @@
 from blue_zero.env.mode0 import BlueMode0
 from blue_zero.env.mode3 import BlueMode3
+from blue_zero.env.mode4 import BlueMode4
 from dataclasses import dataclass, asdict
 from simple_parsing import choice
 
 
 __all__ = []
-__all__.extend(['env_cls', 'ModeOptions', 'Mode3Options'])
+__all__.extend(['env_cls', 'ModeOptions', 'Mode3Options', 'Mode4Options'])
 
 
 env_cls = {
     0: BlueMode0,
-    3: BlueMode3
+    3: BlueMode3,
+    4: BlueMode4,
 }
 
 
@@ -23,9 +25,16 @@ class Mode3Options:
 
 
 @dataclass
+class Mode4Options:
+    """ options for mode 4 """
+    # noodle dimension
+    K: float = 2.0
+
+@dataclass
 class ModeOptions:
     """" options for specific BLUE environments """
     mode3: Mode3Options = Mode3Options()
+    mode4: Mode4Options = Mode4Options()
 
     def get_kwargs(self, mode:int):
         """ Extract the options for a specific mode as a dictionary
