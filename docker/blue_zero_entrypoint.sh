487151f588ecb64084c113322adae1ae20194cd5
micha <mmdanziger@gmail.com>
2021-04-08 22:44:55 +0300

    pull from noodle_mode branch

--- docker/blue_zero_entrypoint.sh
+++ docker/blue_zero_entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://${GH_TOKEN}@github.com/spcornelius/blue_zero.git
+git clone https://${GH_TOKEN}@github.com/spcornelius/blue_zero.git:noodle_clean
 cd blue_zero
 conda run -n blue_zero python setup.py install
 echo $BLUE_ZERO_MODE
@@ -10,3 +10,4 @@ echo $BLUE_ZERO_MODE_4_K
 echo $BOARD_SIZE
 conda run -n blue_zero bash ./scripts/fullpipeline.sh
 exec "$@"
+-e GH_TOKEN=661740da01e0fb094c25f65507ede87f008cadcc -e BLUE_ZERO_MODE=4 -e BLUE_ZERO_P=0.65 -e N_TRAIN_BOARDS=1000 -e N_VAL_BOARDS=100 -e BLUE_ZERO_MODE_4_K=1.5 -e BOARD_SIZE=20
