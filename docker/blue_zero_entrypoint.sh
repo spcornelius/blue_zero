d06ec8ffe9c34711486eb8c541a5daa1a21aa7da
mmdanziger <mmdanziger@users.noreply.github.com>
2021-04-08 23:22:21 +0300

    removed junk from entrypoint file

--- docker/blue_zero_entrypoint.sh
+++ docker/blue_zero_entrypoint.sh
@@ -10,4 +10,3 @@ echo $BLUE_ZERO_MODE_4_K
 echo $BOARD_SIZE
 conda run -n blue_zero bash ./scripts/fullpipeline.sh
 exec "$@"
--e GH_TOKEN=661740da01e0fb094c25f65507ede87f008cadcc -e BLUE_ZERO_MODE=4 -e BLUE_ZERO_P=0.65 -e N_TRAIN_BOARDS=1000 -e N_VAL_BOARDS=100 -e BLUE_ZERO_MODE_4_K=1.5 -e BOARD_SIZE=20
